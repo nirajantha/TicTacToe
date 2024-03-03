ed619b5204e7d5a2ed04f6a53cb9b00d9e361498
Nirajan thapa <nnirajanthapaa@gmail.com>
2024-03-03 16:57:49 +0545

    all the features added and the game is finished to build

---
+++ script.js
@@ -0,0 +1,81 @@
+let boxes = document.querySelectorAll(".box");
+let turnX = true;
+let msgDiv = document.querySelector(".msg-modal");
+let msg = document.querySelector("h1");
+let reset = document.querySelector("#reset");
+let newGameBtn = document.querySelector("#newGame");
+let cancelBtn = document.querySelector("#cancel");
+
+let winPattern = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8]
+]
+
+boxes.forEach((box)=>{
+    box.addEventListener('click',()=>{
+        if(turnX){
+            box.innerText = "X";
+            turnX = false
+        }else{
+            box.innerText = '0';
+            turnX = true;
+        }
+        box.disabled = true;
+        checkwinner();
+    })
+
+})
+ const disableBtn = ()=>{
+    for(let box of boxes){
+        box.disabled = true;
+    }
+ }
+
+ const enableBtn = ()=>{
+    for(let box of boxes){
+        box.disabled = false;
+        box.innerText = "";
+    }
+ }
+
+ const resetGame = ()=>{
+    turnX = true;
+    enableBtn();
+    }
+
+const checkwinner = ()=>{
+   for(let pattern  of winPattern){
+   let pos1 = boxes[pattern[0]].innerText;
+   let pos2 = boxes[pattern[1]].innerText;
+   let pos3 =  boxes[pattern[2]].innerText;
+    if(pos1 != "" && pos2 != "" && pos2 != "" ){
+        if(pos1 ===pos2 && pos2=== pos3){
+            msgDiv.style.display = "flex";
+            msg.innerText = `The winner is ${pos1}`;
+            disableBtn();
+        }
+    }
+   }
+}
+
+reset.addEventListener("click",()=>{
+    resetGame();
+});
+
+newGameBtn.addEventListener("click",()=>{
+    resetGame();
+
+    msgDiv.style.display = "none";
+
+});
+
+cancelBtn.addEventListener("click",()=>{
+    msgDiv.style.display = "none";
+
+});
